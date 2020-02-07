e60bda063bac82e434665965430dc742101de83d
Sashavoshenko2018 <45271236+Sashavoshenko2018@users.noreply.github.com>
2020-02-07 12:18:26 +0200

    MCPP

--- src/mcpp/network/protocol/Info.php
+++ src/mcpp/network/protocol/Info.php
@@ -2,10 +2,8 @@
 
 /*
  *
- *  _____     ____      ______          _____      _____
- * |           \/        |     |   ____|         |          |     |          |
- *|         | \/|      |     |    | ____       |____ |      |____ |
-*|____|     |__ |     |_______|      |                 |
+ * MCPP
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
